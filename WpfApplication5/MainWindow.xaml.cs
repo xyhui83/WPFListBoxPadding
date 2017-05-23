5a59b41acbf974bba0c0e5701ea5d43c913e5e11
xyhui83 <xyhui83@qq.com>
2017-05-23 18:19:01 +0800

    项目初始化

---
+++ WpfApplication5/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication5
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("btnClick");
+
+            //e.Handled = true;
+        }
+
+        private void Grid_Click(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Grid_Click");
+        }
+
+        private void Grid_out_Click(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Grid_out_Click");
+        }
+
+        private void windowClick(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("windowClick");
+        }
+    }
+}
---
+++ WpfApplication5/Window1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication5
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+
+            initData();
+        }
+
+        private void initData()
+        {
+            List<int> list = new List<int>()
+            {
+                1,2,3,4,5,6,7,8,9
+            };
+
+            this.listbox.ItemsSource = list;
+        }
+    }
+}
